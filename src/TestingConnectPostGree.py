60733974bcd5db9d5e0cbd4fa01cede01236465e
tiago de lara ribeiro <laratecsys@gmail.com>
2020-07-10 21:59:19 -0300

    utilizando pyscopg2.connect para conectar a base de dados

--- src/TestingConnectPostGree.py
+++ src/TestingConnectPostGree.py
@@ -3,5 +3,34 @@ Created on 10 de jul de 2020
 
 @author: larat
 '''
+from psycopg2._psycopg import cursor
 
-print('Hello Word');
+print('Hello Postgre')
+import psycopg2
+
+#Connect to the db
+con = psycopg2.connect(
+    host = "localhost",
+    database = "test",
+    user = "postgres",
+    password = "1s1@f23ty"
+)
+
+#Cursor
+cur = con.cursor()
+
+#execute query
+cur.execute("select * from mainTable")
+
+
+rows = cur.fetchall()
+
+for r in rows:
+    print(f"capsule {r[0]} post {r[1]}")
+
+#close the connection
+con.close()
+
+
+
+
